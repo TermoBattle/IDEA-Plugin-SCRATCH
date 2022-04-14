d34fde915e475f1de5bb269ad1f1b5babcb9bd49
GitHub Action <action@github.com>
2022-04-14 19:21:07 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/termobattle/ideapluginscratch/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.termobattle.ideapluginscratch.services
+
+import com.github.termobattle.ideapluginscratch.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
